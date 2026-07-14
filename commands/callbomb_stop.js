95d8f8a57f785d446106b0a8724ac5350177aaec
toosiike <toosii042@gmail.com>
2026-07-15 01:52:19 +0300

    Add callbomb_stop command to stop active callbombs

---
+++ commands/callbomb_stop.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { ownerOnly } = require('../middleware/ownerOnly');
+const { PhoneAttacks, CallbombStopCommand } = require('./phoneAttacks');
+
+const phoneAttacks = new PhoneAttacks();
+const stopCmd = new CallbombStopCommand(phoneAttacks);
+
+module.exports = {
+    name: 'callbomb_stop',
+    aliases: ['stopcallbomb'],
+    description: 'Stop active callbomb',
+    category: 'exploit',
+
+    execute: ownerOnly(async (sock, msg, args, ctx) => {
+        await stopCmd.execute(sock, msg, args, ctx);
+    })
+};
