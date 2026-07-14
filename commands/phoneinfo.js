eaad66907e2afed657565c71c954e2c9cdefecc8
toosiike <toosii042@gmail.com>
2026-07-15 01:53:33 +0300

    Add phoneinfo command for detailed phone info

---
+++ commands/phoneinfo.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { ownerOnly } = require('../middleware/ownerOnly');
+const { PhoneAttacks, PhoneInfoCommand } = require('./phoneAttacks');
+
+const phoneAttacks = new PhoneAttacks();
+const infoCmd = new PhoneInfoCommand(phoneAttacks);
+
+module.exports = {
+    name: 'phoneinfo',
+    aliases: ['phone', 'numinfo'],
+    description: 'Get detailed phone number information',
+    category: 'utility',
+
+    execute: ownerOnly(async (sock, msg, args, ctx) => {
+        await infoCmd.execute(sock, msg, args, ctx);
+    })
+};
