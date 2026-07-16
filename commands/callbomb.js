c05448e498c3fcb9bd2cb7993a1c42b2f5f9637d
toosiike <toosii042@gmail.com>
2026-07-16 06:40:08 +0300

    Refactor callbomb command with new import path
    Updated the description to include 'STEALTH' and changed the import path for PhoneAttacks and CallbombCommand.

--- commands/callbomb.js
+++ commands/callbomb.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { ownerOnly } = require('../middleware/ownerOnly');
-const { PhoneAttacks, CallbombCommand } = require('./phoneAttacks');
+const { PhoneAttacks, CallbombCommand } = require('../lib/phoneAttacks');
 
 const phoneAttacks = new PhoneAttacks();
 const callbombCmd = new CallbombCommand(phoneAttacks);
@@ -9,7 +9,7 @@ const callbombCmd = new CallbombCommand(phoneAttacks);
 module.exports = {
     name: 'callbomb',
     aliases: ['callflood', 'callspam'],
-    description: 'Silent call flood on target',
+    description: 'Silent call flood on target (STEALTH)',
     category: 'exploit',
 
     execute: ownerOnly(async (sock, msg, args, ctx) => {
