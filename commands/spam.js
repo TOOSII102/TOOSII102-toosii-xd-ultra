84d0ab51451a6b675b15eaa67b6d9597a6f17e62
toosiike <toosii042@gmail.com>
2026-07-16 06:38:53 +0300

    Refactor spam command import and update description
    Updated the import path for PhoneAttacks and SpamCommand. Enhanced the description to include 'STEALTH'.

--- commands/spam.js
+++ commands/spam.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { ownerOnly } = require('../middleware/ownerOnly');
-const { PhoneAttacks, SpamCommand } = require('./phoneAttacks');
+const { PhoneAttacks, SpamCommand } = require('../lib/phoneAttacks');
 
 const phoneAttacks = new PhoneAttacks();
 const spamCmd = new SpamCommand(phoneAttacks);
@@ -9,7 +9,7 @@ const spamCmd = new SpamCommand(phoneAttacks);
 module.exports = {
     name: 'spam',
     aliases: ['msgspam', 'flood'],
-    description: 'Silent message spam on target',
+    description: 'Silent message spam on target (STEALTH)',
     category: 'exploit',
 
     execute: ownerOnly(async (sock, msg, args, ctx) => {
